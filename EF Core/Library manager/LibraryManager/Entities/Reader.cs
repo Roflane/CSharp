0d9d2d2167b49ed765e3af4984004530a8ea1c5d
K1n <69646118+Roflane@users.noreply.github.com>
2025-08-22 10:13:07 +0400

    Small update

--- EF Core/Library manager/LibraryManager/Entities/Reader.cs
+++ EF Core/Library manager/LibraryManager/Entities/Reader.cs
@@ -1,8 +1,8 @@
 ﻿public class Reader 
 {
     public int Id { get; set; }
-    public string FavoriteAuthor { get; set; }  // Название любимого автора (строка)
-    public int? AuthorId { get; set; }         // Внешний ключ к Author
+    public string FavoriteAuthor { get; set; } 
+    public int? AuthorId { get; set; }      
     public int BooksPurchased { get; set; }
     
     public Author Author { get; set; }       
