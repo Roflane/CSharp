b62e9afc045c0ccfec028f12899f74b75e4c8383
K1n <69646118+Roflane@users.noreply.github.com>
2025-08-28 15:56:34 +0400

    Create

---
+++ Launcher/AppLauncher/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace AppLauncher;
+
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+        Name = "AppLauncher";
+        
+        if (Checker.IsAppRunning(Name, 1)) this.Close();
+        LauncherCore.Instance.PushArgs();
+    }
+    
+    private void ButtonClose_OnClick(object sender, RoutedEventArgs e) {
+        Application.Current.Shutdown();
+    }
+
+    private void ButtonStart_OnClick(object sender, RoutedEventArgs e) {
+        LauncherCore.Instance.LaunchApp();
+    }
+}
