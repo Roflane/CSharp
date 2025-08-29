af3e82dd28c37ac5595e6b16d16e70805266b39f
K1n <69646118+Roflane@users.noreply.github.com>
2025-08-29 22:04:27 +0400

    X

--- Launcher/AppLauncher/Misc/Response.cs
+++
@@ -1,5 +0,0 @@
-﻿public static class Response {
-    public static readonly string ErrorParse = "Failed to parse arg.";
-    public static readonly string ErrorAppLaunch = "Failed to launch app.";
-    public static readonly string ErrorAppIsRunning = "Unable to relaunch app.";
-}
