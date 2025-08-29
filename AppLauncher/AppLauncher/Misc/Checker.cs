f79649cf3d81e6d348921303f94fb07bcc808b9b
K1n <69646118+Roflane@users.noreply.github.com>
2025-08-29 22:05:42 +0400

    Push

---
+++ AppLauncher/AppLauncher/Misc/Checker.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics;
+
+public static class Checker {
+    public static bool IsAppRunning(string app, int count = 0) {
+        var res = Process.GetProcessesByName(app);
+        return res.Length > count;
+    }
+}
---
+++ AppLauncher/AppLauncher/Misc/Response.cs
@@ -0,0 +1,5 @@
+﻿public static class Response {
+    public static readonly string ErrorParse = "Failed to parse arg.";
+    public static readonly string ErrorAppLaunch = "Failed to launch app.";
+    public static readonly string ErrorAppIsRunning = "Unable to relaunch app.";
+}
