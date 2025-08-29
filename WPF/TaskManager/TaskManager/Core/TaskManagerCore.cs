4657f3ae7df6b81dbff41134a1b23cd492a9db52
K1n <69646118+Roflane@users.noreply.github.com>
2025-08-29 22:07:11 +0400

    Cleanup

---
+++ WPF/TaskManager/TaskManager/Core/TaskManagerCore.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Collections.ObjectModel;
+
+namespace TaskManager.Core;
+
+public class ProcessInfo {
+    public string ProcessName { get; set; }
+    public int ProcessId { get; set; } 
+    public string PID => ProcessId.ToString(); 
+}
+
+public class TaskManagerCore {
+    private static TaskManagerCore _instance = new();
+    public static TaskManagerCore Instance => _instance;
+    
+    public ObservableCollection<ProcessInfo> Processes { get; } = new();
+    
+    public void GetProcesses() {
+        Processes.Clear();
+        List<Process> procs = Process.GetProcesses().OrderBy(p => p.Id).ToList();
+        foreach (var proc in procs) {
+            if (proc.ProcessName == "System" && proc.Id == 4) continue;
+            Processes.Add(new ProcessInfo {
+                ProcessName = proc.ProcessName + ".exe",
+                ProcessId = proc.Id
+            });
+        }
+        
+    }
+
+    public void KillProcess(int pid) {
+        Process.GetProcessById(pid).Kill();
+    }
+
+    public void ChangePriority(int pid, ProcessPriorityClass priority) {
+        Process.GetProcessById(pid).PriorityClass = priority;  
+    }
+
+    public void SetAffinityOne(int pid) {
+        Process.GetProcessById(pid).ProcessorAffinity = new IntPtr(0b1);
+    }
+    
+    public void SetAffinityFull(int pid) {
+        Process.GetProcessById(pid).ProcessorAffinity = new IntPtr(1L << Environment.ProcessorCount) - 1;
+    }
+}
---
+++ WPF/TaskManager/TaskManager/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+using TaskManager.Core;
+namespace TaskManager;
+
+public static class WrapperEvent {
+    public static void Priority(object sender, ProcessPriorityClass priority) {
+        try {
+            if (sender is MenuItem menuItem) {
+                if (menuItem.CommandParameter is ProcessInfo processInfo) {
+                    TaskManagerCore.Instance.ChangePriority(processInfo.ProcessId, priority);
+                }
+            }
+        }
+        catch (Exception ex) {
+            MessageBox.Show($"{ex.Message}",  "Error", 
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
+public partial class MainWindow : Window {
+    private DispatcherTimer _timer = new DispatcherTimer();
+    public MainWindow() {
+        InitializeComponent();
+         
+        DataContext = TaskManagerCore.Instance;
+        ProcessListBox.ItemsSource = TaskManagerCore.Instance.Processes;
+        
+        var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+        timer.Tick += (s, e) => TaskManagerCore.Instance.GetProcesses();
+        timer.Start();
+    }
+    
+    private void ButtonClose_OnClick(object sender, RoutedEventArgs e) {
+        Application.Current.Shutdown();
+    }
+
+    private void Kill_OnClick(object sender, RoutedEventArgs e) {
+        try {
+            if (sender is MenuItem menuItem) {
+                if (menuItem.CommandParameter is ProcessInfo processInfo) {
+                    TaskManagerCore.Instance.KillProcess(processInfo.ProcessId);
+                }
+            }
+        }
+        catch (Exception ex) {
+            MessageBox.Show($"{ex.Message}",  "Error", 
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    private void PriorityRealtime_OnClick(object sender, RoutedEventArgs e) {
+        WrapperEvent.Priority(sender, ProcessPriorityClass.RealTime);
+    }
+    
+    private void PriorityHigh_OnClick(object sender, RoutedEventArgs e) {
+        WrapperEvent.Priority(sender, ProcessPriorityClass.High);
+    }
+
+    private void PriorityAboveNormal_OnClick(object sender, RoutedEventArgs e) {
+        WrapperEvent.Priority(sender, ProcessPriorityClass.AboveNormal);
+    }
+    
+    private void PriorityNormal_OnClick(object sender, RoutedEventArgs e) {
+        WrapperEvent.Priority(sender, ProcessPriorityClass.Normal);
+    }
+    
+    private void PriorityBelowNormal_OnClick(object sender, RoutedEventArgs e) {
+        WrapperEvent.Priority(sender, ProcessPriorityClass.BelowNormal);
+    }
+    
+    private void PriorityIdle_OnClick(object sender, RoutedEventArgs e) {
+        WrapperEvent.Priority(sender, ProcessPriorityClass.Idle);
+    }
+
+    private void AffinityOne_OnClick(object sender, RoutedEventArgs e) {
+        try {
+            if (sender is MenuItem menuItem) {
+                if (menuItem.CommandParameter is ProcessInfo processInfo) {
+                    TaskManagerCore.Instance.SetAffinityOne(processInfo.ProcessId);
+                }
+            }
+        }
+        catch (Exception ex) {
+            MessageBox.Show($"{ex.Message}",  "Error", 
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+    
+    private void AffinityFull_OnClick(object sender, RoutedEventArgs e) {
+        try {
+            if (sender is MenuItem menuItem) {
+                if (menuItem.CommandParameter is ProcessInfo processInfo) {
+                    TaskManagerCore.Instance.SetAffinityFull(processInfo.ProcessId);
+                }
+            }
+        }
+        catch (Exception ex) {
+            MessageBox.Show($"{ex.Message}",  "Error", 
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
