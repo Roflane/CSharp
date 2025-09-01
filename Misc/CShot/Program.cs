a6a99e71a33323ad89066a3927123882b72c39a1
K1n <69646118+Roflane@users.noreply.github.com>
2025-09-01 18:43:33 +0400

    Push

---
+++ Misc/CShot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+
+class Program {
+    private static readonly string _fn = "screen.png";
+    
+    static void Main() {
+        Console.WriteLine("Press 'PrtSc' to take a screenshot.");
+        while (true) {
+            if (CShot.GetAsyncKeyState(0x2C) != 0) {
+                CShot.TakeScreenshot(_fn);
+                Console.WriteLine("Screenshot has been saved!");
+                Thread.Sleep(800);
+            }
+            Thread.Sleep(73);
+        }
+    }
+}
+
+public static class CShot {
+    [DllImport("CShot.dll")]
+    public static extern void TakeScreenshot(string fileName);
+    
+    [DllImport("User32.dll")]
+    public static extern short GetAsyncKeyState(int vk);
+}
