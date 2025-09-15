851de5a95ce207dd9d12b49d38d17c6175dedea4
Roflane <irasul025@gmail.com>
2025-09-15 17:12:51 +0400

    Push

---
+++ Misc/RAL/Calc/CalcCore.cs
@@ -0,0 +1,39 @@
+﻿namespace Calc;
+
+public static class CalcCore {
+    public static double Sum(params double[] numbers) {
+        double res = 0;
+        foreach (var number in numbers) {
+            res += number;
+        }
+
+        return res;
+    }
+    
+    public static double Sub(params double[] numbers) {
+        double res = numbers[0];
+        foreach (var number in numbers) {
+            res -= number;
+        }
+
+        return res;
+    }
+    
+    public static double Mult(params double[] numbers) {
+        double res = numbers[0];
+        foreach (var number in numbers) {
+            res *= number;
+        }
+    
+        return res;
+    }
+    
+    public static double Div(params double[] numbers) {
+        double res = numbers[0];
+        foreach (var number in numbers) {
+            res /= number;
+        }
+    
+        return res;
+    }
+}
---
+++ Misc/RAL/DummyCalc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.Loader;
+
+namespace DummyCalc;
+
+class Program {
+    static void Main() {
+        string currentDir = Directory.GetCurrentDirectory() + "\\Calc.dll";
+        var assembly = Assembly.LoadFile(currentDir);
+        var type = assembly.GetType("Calc.CalcCore");
+        var method = type.GetMethods().First(s => s.Name == "Div");
+        var res = method.Invoke(null, [new double[] { 1, 2, 3, 4 }]);
+        Console.WriteLine(res);
+    }
+}
